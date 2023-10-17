25a88e7f330a08b429550e5f843e3c17a65d2e12
David Lee <euntek95@gmail.com>
2023-10-17 10:07:14 -0700

    Change if/else to switch statements

--- myscripts.js
+++ myscripts.js
@@ -1,4 +1,7 @@
-for (let j=0; j<5; j++) {
+let playerScore = 0;
+let computerScore = 0;
+
+for (let j=0; j<5; j++) { //Loops game 5x
       
 
 function getComputerChoice(min, max) {
@@ -15,28 +18,39 @@ let computerSelection;
         } else (computerSelection = "scissors");
 console.log(computerSelection);
 
-
 let userInput = prompt("Rock Paper Scissors!").toLowerCase(); //.toLowerCase causes all inputted strings and converts them to lower case
-function game (userInput, computerSelection) {
-        if (userInput === computerSelection) {
+function gamePlay (userInput, computerSelection) {
+        if (userInput === computerSelection) { //Could use switch statement to simplify code
                 prompt("Tied!");
-        } else if ((userInput == "rock") && (computerSelection == "scissors")) {
-                prompt ("You win! Rock beats scissors!");
+        } else if ((userInput == "rock") && (computerSelection == "scissors")) { //To add score in the prompt, add string after computer/playerscore
+                (prompt ("You win! Rock beats scissors!") + playerScore++);
         } else if ((userInput == "rock") && (computerSelection == "paper")) {
-                prompt ("You lose! Paper beats rock!");
+                (prompt ("You lose! Paper beats rock!") + computerScore++);
         } else if ((userInput == "paper") && (computerSelection == "rock")) {
-                prompt ("You win! Paper beats rock!"); 
+                (prompt ("You win! Paper beats rock!") + playerScore++); 
         } else if ((userInput == "paper") && (computerSelection == "scissors")) {
-                prompt ("You lose! Scissors beats paper!"); 
+                (prompt ("You lose! Scissors beats paper!") + computerScore++); 
         } else if ((userInput == "scissors") && (computerSelection == "rock")) {
-                prompt ("You lose! Rock beats scissors");
-        } else { ((userInput == "scissors") && (computerSelection == "paper")) 
-                prompt ("You win! Scissors beats paper!")
+                (prompt ("You lose! Rock beats scissors!") + computerScore++);
+        } else { ((userInput == "scissors") && (computerSelection == "paper")); 
+                (prompt ("You win! Scissors beats paper!") + playerScore++);
         }
-}
- game (userInput, computerSelection);
 
+        console.log ("Computer score" +" "+ computerScore);
+        console.log ("Player score" +" "+ playerScore);
 }
+ gamePlay (userInput, computerSelection);
+}
+
+console.log ("Game over!")
+        if (playerScore > computerScore) {
+                prompt ("Player Wins!");
+        } else if (playerScore == computerScore) {
+                prompt ("Tied!");
+        } else { prompt ("Computer Wins!")
+        }
+
+
 
 
 
